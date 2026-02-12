7b72b4ab89b181c9965e61ffbd9a43a37963d761
VanshByte <shivaji7005dh@gmail.com>
2026-02-12 20:32:59 +0530

    Day12

---
+++ 12.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int matrix[100][100];
+
+    scanf("%d %d", &m, &n);
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    if (m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+    return 0;
+}
